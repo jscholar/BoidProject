33d4d0f0023d88441e19ed140596d35826e9b8c4
ale1 <ale1@eecs.wsu.edu>
2021-05-23 11:05:23 -0700

    Experimenting with boid movement

---
+++ boid.js
@@ -0,0 +1,30 @@
+// Boid object
+function Boid(id) {
+    this.id = id;
+    // Model position
+    this.positionX = Math.random() * 250;
+    this.positionY = Math.random() * 250;
+    // Model velocity
+    this.velocityX = Math.random() * (Math.random() < 0.5 ? -1 : 1);
+    this.velocityY = Math.random() * (Math.random() < 0.5 ? -1 : 1);
+}
+
+// Create a boid div object and randomizes its position on the screen
+function createBoid(boid) {
+    // Create div object using boid object.
+    const newBoid = document.createElement("div");
+    newBoid.setAttribute("id", "boid" + boid.id);
+    newBoid.style.width = 0;
+    newBoid.style.height = 0;
+    newBoid.style.borderTop = "10px solid transparent";
+    newBoid.style.borderBottom = "10px solid transparent";
+    newBoid.style.borderLeft = "35px solid rgb(62, 152, 255)";
+    document.body.appendChild(newBoid);
+    // Randomize the position of the boid on the screen.
+    const angle = -Math.atan2(boid.velocityY, boid.velocityX);
+    const transformValues = [];
+    transformValues.push(`translate(${boid.positionX}px, ${boid.positionY}px)`);
+    transformValues.push(`rotateZ(${angle}rad)`);
+    newBoid.style.transform = transformValues.join(" ");
+}
+export { Boid, createBoid };
--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-// Model
-const boid = document.querySelector("#boid-0");
+import { Boid, createBoid } from './boid.js';
 
-function randomizeBoid() {
+// Initialize boid
+let boid0 = new Boid(0);
+createBoid(boid0);
 
-    // Model position
-    const positionX = Math.random() * 500;
-    const positionY = Math.random() * 500;
+// Boid information for debugging
+// console.log("Client width: " + document.documentElement.clientWidth);
+// console.log("Client height: " + document.documentElement.clientHeight);
+// console.log("boid id: " + boid0.id);
+// console.log("position x: " + boid0.positionX);
+// console.log("position y: " + boid0.positionY);
+// console.log("velocity x: " + boid0.velocityX);
+// console.log("velocity y: " + boid0.velocityY);
 
+// Angle, speed, acceleration
+// let angle = -Math.atan2(boid0.velocityY, boid0.velocityX);
+let speed = 1;
+let acceleration = 1;
 
-    // Model velocity
-    var plusOrMinusX = Math.random() < 0.5 ? -1 : 1;
-    var plusOrMinusY = Math.random() < 0.5 ? -1 : 1;
-    const velocityX = Math.random() * plusOrMinusX;
-    const velocityY = Math.random() * plusOrMinusY;
+// border
+let ww = document.documentElement.clientWidth - 100;
+let hh = document.documentElement.clientHeight - 100;
 
-    // Model angle
-    const angle = -Math.atan2(velocityY, velocityX);
-
-    /* View */
-
-    const transformValues = [];
-
-    // Set initial position of boid
-    transformValues.push(`translate(${positionX}px, ${positionY}px)`);
-
-    // Set initial rotation of the boid
-    transformValues.push(`rotateZ(${angle}rad)`);
-
-    // Calculate the translation
-
-
-    // Calculate the rotation
-
-    boid.style.transform = transformValues.join(" ");
-
+// Testing boid movement
+function moveBoid(boid) {
+    let angle = -Math.atan2(boid.velocityY, boid.velocityX);
+    speed += acceleration;
+    var currentBoid = document.querySelector(`#boid${boid.id}`);
+    let xtransform = boid.positionX + (speed * Math.cos(angle));
+    let ytransform = boid.positionY + (speed * Math.sin(angle));
+    let ztransform = -Math.atan2(boid.velocityY, boid.velocityX);
+    currentBoid.style.transform = `translate(${xtransform}px, ${ytransform}px) rotateZ(${ztransform}rad)`;
+    if ((xtransform > 0 && xtransform < ww) && (ytransform > 0 && ytransform < hh)) {
+        window.requestAnimationFrame(moveBoid.bind(window, boid));
+        // console.log("position x: " + xtransform + "  position y: " + ytransform);
+    }
 }
 
-randomizeBoid();
+let boid1 = new Boid(1);
+let boid2 = new Boid(2);
+let boid3 = new Boid(3);
+let boid4 = new Boid(4);
+let boid5 = new Boid(5);
+let boid6 = new Boid(6);
+let boid7 = new Boid(7);
+let boid8 = new Boid(8);
+let boid9 = new Boid(9);
+createBoid(boid1);
+createBoid(boid2);
+createBoid(boid3);
+createBoid(boid4);
+createBoid(boid5);
+createBoid(boid6);
+createBoid(boid7);
+createBoid(boid8);
+createBoid(boid9);
+window.requestAnimationFrame(moveBoid.bind(window, boid0));
+window.requestAnimationFrame(moveBoid.bind(window, boid1));
+window.requestAnimationFrame(moveBoid.bind(window, boid2));
+window.requestAnimationFrame(moveBoid.bind(window, boid3));
+window.requestAnimationFrame(moveBoid.bind(window, boid4));
+window.requestAnimationFrame(moveBoid.bind(window, boid5));
+window.requestAnimationFrame(moveBoid.bind(window, boid6));
+window.requestAnimationFrame(moveBoid.bind(window, boid7));
+window.requestAnimationFrame(moveBoid.bind(window, boid8));
+window.requestAnimationFrame(moveBoid.bind(window, boid9));
